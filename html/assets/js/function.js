5e53e44a82fccb451d116520b9476e20c4366324
dev-jtkim <juntaezz1@gmail.com>
2024-06-10 13:21:00 +0900

    assets update

--- html/assets/js/function.js
+++ html/assets/js/function.js
@@ -1,48 +1,5 @@
 "use strict";
 
-/*
-const metaLink = document.createElement('meta');
-metaLink.setAttribute('name', 'theme-color');
-metaLink.content = "#ffffff";
-document.getElementsByTagName('head')[0].prepend(metaLink);
-*/
-
-
-
-
-
-/** 브라우저의 스크린 Height 값을 구함 : 로그인, 회원가입 레이아웃에 사용
-function setScreenSize() {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-setScreenSize();
-window.addEventListener('resize', () => setScreenSize());
-*/
-
-let sbartrigger = document.querySelector('.js-drawer-trigger');
-let sbarclose = document.querySelector('.js-drawer-close');
-let sidebar = document.querySelector('.sidebar');
-let dimlayer = document.querySelector('.dim-layer');
-
-if(sidebar){
-  let classOpen = [sidebar, dimlayer];
-
-  sbartrigger.addEventListener('click', function(e){
-    classOpen.forEach(e => e.classList.add('is-active'));
-    document.body.classList.add('overflow-hidden');
-  });
-
-  let classCloseClick = [dimlayer, sbarclose];
-  classCloseClick.forEach(function(el) {
-    el.addEventListener('click', function(els) {
-      classOpen.forEach(els => els.classList.remove('is-active'));
-      document.body.classList.remove('overflow-hidden');
-    });
-  });
-}
-
-
 /* Modal */
 document.addEventListener('click', function (e) {
   e = e || window.event;
@@ -60,6 +17,37 @@ document.addEventListener('click', function (e) {
       var modal = document.querySelector('[class="modal open"]');
       modal.classList.remove('open');
       e.preventDefault();
+  }
+}, false);
+
+
+/* Bottom Sheet */
+document.addEventListener('click', function (e) {
+  e = e || window.event;
+  var target = e.target || e.srcElement;
+
+  if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'bottom-sheet') {
+      if (target.hasAttribute('data-target')) {
+          var bt_ID = target.getAttribute('data-target');
+          document.getElementById(bt_ID).classList.add('open');
+          document.body.classList.add('overflow-hidden');
+          e.preventDefault();
+      }
+  }
+
+  //if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'bottom-sheet') || target.classList.contains('bottom-sheet')) {
+  if (target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'bottom-sheet') {
+      var bottomSheet = document.querySelector('.bottom-sheet.open');
+      if (bottomSheet) {
+          bottomSheet.classList.add('hide');
+
+          bottomSheet.addEventListener('transitionend', function handler() {
+              bottomSheet.classList.remove('open');
+              bottomSheet.classList.remove('hide');
+              document.body.classList.remove('overflow-hidden');
+              bottomSheet.removeEventListener('transitionend', handler);
+          });
+      }
   }
 }, false);
 
@@ -140,38 +128,75 @@ document.addEventListener("click", function (e) {
 const buttonBoxElements = document.querySelectorAll(".button-dropdown__inner");
 
 function toggleButtonBox(buttonBox) {
+  // 모든 버튼 박스를 닫는다
+  buttonBoxElements.forEach(boxElement => {
+    if (boxElement !== buttonBox) {
+      boxElement.classList.remove("expanded");
+    }
+  });
+
+  // 클릭된 버튼 박스의 상태를 토글한다
   buttonBox.classList.toggle("expanded");
 }
 
-buttonBoxElements.forEach(buttonBoxElements => {
-  buttonBoxElements.addEventListener("click", function (e) {
-    toggleButtonBox(buttonBoxElements);
+// 각 버튼 박스에 클릭 이벤트 핸들러를 추가한다
+buttonBoxElements.forEach(buttonBox => {
+  buttonBox.addEventListener("click", function (e) {
+    //e.stopPropagation(); // 이벤트가 상위 요소로 전파되는 것을 막는다
+    toggleButtonBox(buttonBox);
   });
 });
 
+// 문서 전체에 클릭 이벤트 핸들러를 추가한다
 document.addEventListener("click", function (e) {
   const targetElement = e.target;
   const isSelect = targetElement.classList.contains("button-dropdown__inner") || targetElement.closest(".button-dropdown__inner");
 
-  if (isSelect) {
-    return;
+  if (!isSelect) {
+    // 버튼 박스 외부를 클릭했을 때 모든 버튼 박스를 닫는다
+    buttonBoxElements.forEach(boxElement => {
+      boxElement.classList.remove("expanded");
+    });
   }
-
-  const buttonBoxElements = document.querySelectorAll(".button-dropdown__inner");
-
-  buttonBoxElements.forEach(boxElement => {
-    boxElement.classList.remove("expanded");
-  });
 });
 
 
-/* 인풋 초기화 */
-const clearInput = document.getElementsByClassName('field-group__clear');
-for(let i = 0; i < clearInput.length; i++){
-  (function(i){
-    clearInput[i].onclick = function(){
-      clearInput[i].parentNode.getElementsByClassName('field-group__input')[0].value = ''; 
-    };
-  })(i);
+/* INPUT Clear */
+const clearInputs = document.getElementsByClassName('js-input-clear');
+for (let i = 0; i < clearInputs.length; i++) {
+  clearInputs[i].onclick = function () {
+    const parent = clearInputs[i].parentNode;
+    const fieldGroup1 = parent.getElementsByClassName('field-group__input')[0];
+    const fieldGroup2 = parent.getElementsByClassName('field-group2__input')[0];
+    
+    if (fieldGroup1) {
+      fieldGroup1.value = '';
+    }
+    
+    if (fieldGroup2) {
+      fieldGroup2.value = '';
+    }
+  };
 }
 
+
+/* TEXTAREA Height Auto */
+function autoResizeTextarea() {
+  const textareas = document.querySelectorAll('.js-auto-resize');
+  if (!textareas.length) return;
+
+  function autoResize(event) {
+    const textarea = event.target;
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+  }
+
+  textareas.forEach(textarea => {
+    textarea.addEventListener('input', autoResize);
+    // 초기 높이 설정
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+  });
+}
+
+document.addEventListener('DOMContentLoaded', autoResizeTextarea);
