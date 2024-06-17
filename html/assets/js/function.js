45c6c923e6ee2f96a5f858a5b3b4f0b03f30274d
limgeontae <taelim@ilark.co.kr>
2024-06-17 11:46:02 +0900

    publish update 240614

--- html/assets/js/function.js
+++ html/assets/js/function.js
@@ -20,7 +20,6 @@ document.addEventListener('click', function (e) {
   }
 }, false);
 
-
 /* Bottom Sheet */
 document.addEventListener('click', function (e) {
   e = e || window.event;
@@ -35,7 +34,6 @@ document.addEventListener('click', function (e) {
       }
   }
 
-  //if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'bottom-sheet') || target.classList.contains('bottom-sheet')) {
   if (target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'bottom-sheet') {
       var bottomSheet = document.querySelector('.bottom-sheet.open');
       if (bottomSheet) {
@@ -50,7 +48,6 @@ document.addEventListener('click', function (e) {
       }
   }
 }, false);
-
 
 /* 아코디언 게시판 */
 const accordions = document.querySelectorAll(".accordion__item");
@@ -75,7 +72,6 @@ accordions.forEach((accordion) => {
 		if (content.style.maxHeight) {
 			closeAccordion(accordion);
 		} else {
-			//accordions.forEach((accordion) => closeAccordion(accordion));
 			openAccordion(accordion);
 		}
 	};
@@ -83,44 +79,58 @@ accordions.forEach((accordion) => {
 
 
 /* 커스텀 셀렉트 */
-const selectBoxElements = document.querySelectorAll(".select-dropdown__inner");
+function initializeCustomSelect() {
+  const selectBoxElements = document.querySelectorAll(".select-dropdown__inner");
 
-function toggleSelectBox(selectBox) {
-  selectBox.classList.toggle("expanded");
-}
+  function toggleSelectBox(selectBox) {
+    const allSelectBoxElements = document.querySelectorAll(".select-dropdown__inner");
 
-function selectOption(optionElement) {
-  const selectBox = optionElement.closest(".select-dropdown__inner");
-  const selectedElement = selectBox.querySelector(".select-dropdown__label");
-  selectedElement.textContent = optionElement.textContent;
-}
+    allSelectBoxElements.forEach(boxElement => {
+      if (boxElement !== selectBox) {
+        boxElement.classList.remove("expanded");
+      }
+    });
 
-selectBoxElements.forEach(selectBoxElement => {
-  selectBoxElement.addEventListener("click", function (e) {
-    const targetElement = e.target;
-    const isOptionElement = targetElement.classList.contains("select-dropdown__item");
-
-    if (isOptionElement) {
-      selectOption(targetElement);
-    }
-
-    toggleSelectBox(selectBoxElement);
-  });
-});
-
-document.addEventListener("click", function (e) {
-  const targetElement = e.target;
-  const isSelect = targetElement.classList.contains("select-dropdown__inner") || targetElement.closest(".select-dropdown__inner");
-
-  if (isSelect) {
-    return;
+    selectBox.classList.toggle("expanded");
   }
 
-  const allSelectBoxElements = document.querySelectorAll(".select-dropdown__inner");
+  function selectOption(optionElement) {
+    const selectBox = optionElement.closest(".select-dropdown__inner");
+    const selectedElement = selectBox.querySelector(".select-dropdown__label");
+    selectedElement.textContent = optionElement.textContent;
+  }
 
-  allSelectBoxElements.forEach(boxElement => {
-    boxElement.classList.remove("expanded");
+  selectBoxElements.forEach(selectBoxElement => {
+    selectBoxElement.addEventListener("click", function (e) {
+      const targetElement = e.target;
+      const isOptionElement = targetElement.classList.contains("select-dropdown__item");
+
+      if (isOptionElement) {
+        selectOption(targetElement);
+      }
+
+      toggleSelectBox(selectBoxElement);
+    });
   });
+
+  document.addEventListener("click", function (e) {
+    const targetElement = e.target;
+    const isSelect = targetElement.classList.contains("select-dropdown__inner") || targetElement.closest(".select-dropdown__inner");
+
+    if (isSelect) {
+      return;
+    }
+
+    const allSelectBoxElements = document.querySelectorAll(".select-dropdown__inner");
+
+    allSelectBoxElements.forEach(boxElement => {
+      boxElement.classList.remove("expanded");
+    });
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  initializeCustomSelect();
 });
 
 
@@ -128,57 +138,120 @@ document.addEventListener("click", function (e) {
 const buttonBoxElements = document.querySelectorAll(".button-dropdown__inner");
 
 function toggleButtonBox(buttonBox) {
-  // 모든 버튼 박스를 닫는다
   buttonBoxElements.forEach(boxElement => {
     if (boxElement !== buttonBox) {
       boxElement.classList.remove("expanded");
     }
   });
 
-  // 클릭된 버튼 박스의 상태를 토글한다
   buttonBox.classList.toggle("expanded");
 }
 
-// 각 버튼 박스에 클릭 이벤트 핸들러를 추가한다
 buttonBoxElements.forEach(buttonBox => {
   buttonBox.addEventListener("click", function (e) {
-    //e.stopPropagation(); // 이벤트가 상위 요소로 전파되는 것을 막는다
     toggleButtonBox(buttonBox);
   });
 });
 
-// 문서 전체에 클릭 이벤트 핸들러를 추가한다
 document.addEventListener("click", function (e) {
   const targetElement = e.target;
   const isSelect = targetElement.classList.contains("button-dropdown__inner") || targetElement.closest(".button-dropdown__inner");
 
   if (!isSelect) {
-    // 버튼 박스 외부를 클릭했을 때 모든 버튼 박스를 닫는다
     buttonBoxElements.forEach(boxElement => {
       boxElement.classList.remove("expanded");
     });
   }
 });
 
-
 /* INPUT Clear */
-const clearInputs = document.getElementsByClassName('js-input-clear');
-for (let i = 0; i < clearInputs.length; i++) {
-  clearInputs[i].onclick = function () {
-    const parent = clearInputs[i].parentNode;
-    const fieldGroup1 = parent.getElementsByClassName('field-group__input')[0];
-    const fieldGroup2 = parent.getElementsByClassName('field-group2__input')[0];
-    
-    if (fieldGroup1) {
-      fieldGroup1.value = '';
-    }
-    
-    if (fieldGroup2) {
-      fieldGroup2.value = '';
-    }
-  };
+function initializeInputClear() {
+  const clearInputs = document.getElementsByClassName('js-input-clear');
+  for (let i = 0; i < clearInputs.length; i++) {
+    clearInputs[i].onclick = function () {
+      const parent = clearInputs[i].parentNode;
+      const fieldGroup1 = parent.getElementsByClassName('field-group__input')[0];
+      const fieldGroup2 = parent.getElementsByClassName('field-group2__input')[0];
+
+      if (fieldGroup1) {
+        fieldGroup1.value = '';
+      }
+
+      if (fieldGroup2) {
+        fieldGroup2.value = '';
+      }
+    };
+  }
 }
 
+/* Modal - Page Call */
+document.addEventListener('DOMContentLoaded', function() {
+  var modalDialog = document.getElementById('modal-dialog');
+  var modalDcontent = document.querySelector('.modal-dialog__wrapper');
+  var modalDbody = document.getElementById('modalWrapper');
+  var buttons = document.getElementsByClassName('js-openModalBtn');
+
+  Array.prototype.forEach.call(buttons, function(button) {
+      button.addEventListener('click', function() {
+          var url = button.getAttribute('data-url');
+          var xhr = new XMLHttpRequest();
+          xhr.open('GET', url, true);
+          xhr.onreadystatechange = function() {
+              if (xhr.readyState === 4) {
+                  if (xhr.status === 200) {
+                      modalDbody.innerHTML = xhr.responseText;
+                      modalDialog.style.display = 'block';
+                      document.body.classList.add('overflow-hidden');
+                      setTimeout(function() {
+                        modalDcontent.classList.add('open');
+                      }, 10);
+
+                      var closeBtn = modalDbody.querySelector('.modal-dialog__close');
+                      if (closeBtn) {
+                          closeBtn.addEventListener('click', function() {
+                              modalDcontent.classList.remove('open');
+                              document.body.classList.remove('overflow-hidden');
+                              setTimeout(function() {
+                                modalDialog.style.display = 'none';
+                              }, 500);
+                          });
+                      }
+
+                      // 동적으로 로드된 콘텐츠 내의 스크립트를 실행하는 함수
+                      var scripts = modalDbody.getElementsByTagName('script');
+                      for (var i = 0; i < scripts.length; i++) {
+                          eval(scripts[i].textContent);
+                      }                      
+
+                      // 동적으로 로드된 콘텐츠에 대해 autoResizeTextarea 및 js-input-clear 재실행
+                      autoResizeTextarea();
+                      initializeInputClear();
+                      initializeCustomSelect();
+                  } else {
+                      modalDbody.innerHTML = '<p>Failed to load content.</p>';
+                      modalDialog.style.display = 'block';
+                      setTimeout(function() {
+                          modalDcontent.classList.add('show');
+                      }, 10);
+                  }
+              }
+          };
+          xhr.send();
+      });
+  });
+
+  window.addEventListener('click', function(event) {
+      if (event.target == modalDialog) {
+          modalDcontent.classList.remove('show');
+          setTimeout(function() {
+            modalDialog.style.display = 'none';
+          }, 500);
+      }
+  });
+
+  // 초기 페이지 로드 시 input clear 초기화
+  initializeInputClear();
+});
 
 /* TEXTAREA Height Auto */
 function autoResizeTextarea() {
@@ -193,7 +266,6 @@ function autoResizeTextarea() {
 
   textareas.forEach(textarea => {
     textarea.addEventListener('input', autoResize);
-    // 초기 높이 설정
     textarea.style.height = 'auto';
     textarea.style.height = textarea.scrollHeight + 'px';
   });
